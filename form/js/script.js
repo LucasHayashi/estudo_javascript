ba9004e3fcca10019d879642506d12a8342d54fd
Lucas Hayashi <lucashayashi5@gmail.com>
2024-08-10 23:00:30 -0300

    Ajuste na ferramenta de validação de formulário

--- form/js/script.js
+++ form/js/script.js
@@ -1,25 +1,50 @@
 const formulario = document.forms.formulario;
+const btnEnviar = document.getElementById('btnEnviar');
+const resultado = document.getElementById('resultado');
+const valores = document.getElementById('valores');
 
-var valores = {};
+function pegaTodosValores(event) {
+  valores[event.target.name] = event.target.value;
 
-function pegaTodosValores(event){
-    valores[event.target.name] = event.target.value;
-    console.log(valores);
+  valores.innerHTML = JSON.stringify(valores, null, 2);
 }
 
-function validaInput(event){
-    const target = event.target;
+function validaInput(event) {
+  const target = event.target;
 
-    if(!target.checkValidity()) {
-        target.classList.add('invalido');
-        target.nextElementSibling.innerText = target.validationMessage;
-      } else {
-        target.classList.remove('invalido');
-        target.nextElementSibling.innerText = "";
-      }
-    
+  if (!target.checkValidity()) {
+    target.classList.add('invalido');
+    target.nextElementSibling.innerText = target.validationMessage;
+  } else {
+    target.classList.remove('invalido');
+    target.nextElementSibling.innerText = "";
+  }
+
 }
 
-formulario.addEventListener('change',validaInput);
+formulario.addEventListener('change', validaInput);
 
-formulario.addEventListener('change',pegaTodosValores);
+formulario.addEventListener('change', pegaTodosValores);
+
+btnEnviar.addEventListener('click', function (event) {
+  event.preventDefault();
+  const inputs = formulario.querySelectorAll('input');
+  let allValid = true;
+
+  inputs.forEach(input => {
+    if (!input.checkValidity()) {
+      input.classList.add('invalido');
+      input.nextElementSibling.innerText = input.validationMessage;
+      allValid = false;
+    } else {
+      input.classList.remove('invalido');
+      input.nextElementSibling.innerText = '';
+    }
+  });
+
+  if (allValid) {
+    resultado.innerText = 'Tudo certo!';
+  } else {
+    resultado.innerText = 'Há campos inválidos!';
+  }
+});
