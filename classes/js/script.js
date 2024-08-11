da42ef1729327b2f5baa8542a304a02d0a1fbff1
Lucas Hayashi <lucashayashi5@gmail.com>
2024-08-10 21:17:40 -0300

    Adicionado funcionalidade para setar a data fim da promoção manualmente

--- classes/js/script.js
+++ classes/js/script.js
@@ -1,17 +1,38 @@
 import countDown from "./countdown.js";
 
+function startCountdown(dataFutura) {
+    const endSpecialOffer = new countDown(dataFutura);
+    if (timer) {
+        clearInterval(timer);
+    }
+    timer = setInterval(() => {
+        dias.innerText = endSpecialOffer.total.days;
+        horas.innerText = endSpecialOffer.total.hours;
+        minutos.innerText = endSpecialOffer.total.minutes;
+        segundos.innerText = endSpecialOffer.total.seconds;
+    }, 1000);
+}
+
+function stopCountdown() {
+    clearInterval(timer);
+}
+
 let dias = document.querySelector('.dias');
 let horas = document.querySelector('.horas');
 let minutos = document.querySelector('.minutos');
 let segundos = document.querySelector('.segundos');
+let btnIniciar = document.getElementById('btnIniciar');
+let btnParar = document.getElementById('btnParar');
+let timer = null;
 
-let endOfferDate = new Date("01 June 2022 00:00");
+btnIniciar.addEventListener('click', () => {
+    let endOfferDate = document.getElementById('data_fim').value;
 
-const endSpecialOffer = new countDown(endOfferDate);
+    if (endOfferDate == '') {
+        alert('Por favor, preencha a data');
+        return;
+    }
 
-const SpecialOfferTime = setInterval(() => {
-    dias.innerText = endSpecialOffer.total.days;
-    horas.innerText = endSpecialOffer.total.hours;
-    minutos.innerText = endSpecialOffer.total.minutes;
-    segundos.innerText = endSpecialOffer.total.seconds;
-}, 1000);
+    startCountdown(new Date(endOfferDate));
+});
+btnParar.addEventListener('click', stopCountdown);
